f2d9508f26bd5aa7593627b5def6055f83c39074
Thibault Hallouin <thibault.hallouin@ncas.ac.uk>
2020-02-04 09:40:44 +0000

    changes ancil data for dummy surface model

--- cm4twc/components/surface/example.py
+++ cm4twc/components/surface/example.py
@@ -6,7 +6,7 @@ class Dummy(SurfaceComponent):
     def __init__(self):
         super().__init__(driving_data_names=('rainfall', 'snowfall',
                                              'air_temperature',),
-                         ancil_data_names=('leaf_area_index',),
+                         ancil_data_names=('vegetation_fraction',),
                          parameter_names=())
 
     def run(self, rainfall, snowfall, air_temperature,
