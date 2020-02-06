899a865561d38a1be8ed63c3ccadd4731f85bb4a
Thibault Hallouin <thibault.hallouin@ncas.ac.uk>
2020-02-06 09:10:07 +0000

    updates data management by relying on cf-python
    Variable and DataBase classes use cf-python to read Fields in a CF-netCDF files. Variable is a subclass of cf.Field with a simplified interface, DataBase is a dict-like class which stores Variable instances only.

--- cm4twc/data_.py
+++ cm4twc/data_.py
@@ -1,16 +1,17 @@
 from collections.abc import MutableMapping
+import cf
 
 
 class DataBase(MutableMapping):
     """
     Class behaving like a dictionary but restricting the values of
-    its items to (a) specific type(s).
+    its items of type Variable.
     """
 
     def __init__(self, *args, **kwargs):
 
         self.variables = dict()
-        self.update(dict(*args, **kwargs))  # use the free update to set keys
+        self.update(dict(*args, **kwargs))  # use the 'free' update to set keys
 
     def __getitem__(self, key): return self.variables[key]
 
@@ -19,7 +20,8 @@ class DataBase(MutableMapping):
         if isinstance(value, Variable):
             self.variables[key] = value
         else:
-            raise TypeError("The database can only contain Variable instances.")
+            raise TypeError("The database can only contain "
+                            "instances of Variable.")
 
     def __delitem__(self, key): del self.variables[key]
 
@@ -29,10 +31,48 @@ class DataBase(MutableMapping):
 
     def append(self, *args, **kwargs): self.update(*args, **kwargs)
 
+    def update_with_cf_nc_file(self, pathname, mapping=None):
 
-class Variable(object):
+        return self.update(
+            self._get_dict_variables_from_cf_nc_file(pathname, mapping)
+        )
 
-    def __init__(self, name, array=None, *args, **kwargs):
+    @classmethod
+    def from_cf_nc_file(cls, pathname, mapping=None):
 
-        self.standard_name = name
-        self.array = array
+        return cls(
+            cls._get_dict_variables_from_cf_nc_file(pathname, mapping)
+        )
+
+    @staticmethod
+    def _get_dict_variables_from_cf_nc_file(pathname, mapping=None):
+        return {
+            mapping[field.standard_name]
+            if mapping and (field.standard_name in mapping)
+            else field.standard_name: Variable(source=field, copy=False)
+            for field in cf.read(pathname)
+        }
+
+
+class Variable(cf.Field):
+
+    def __init__(self, source, copy=False):
+
+        super(Variable, self).__init__(source=source, copy=copy)
+
+    @classmethod
+    def from_cf_nc_file(cls, variablename, pathname):
+
+        try:
+            field = cf.read(pathname)(variablename)
+        except FileNotFoundError:
+            raise FileNotFoundError("Error during initialisation of {} from "
+                                    "CF-netCDF file: there is no such file or "
+                                    "directory: {}.".format(cls.__name__,
+                                                            pathname))
+
+        if field:
+            return cls(source=field)
+        else:
+            raise ValueError("There is no variable named '{}' in the CF-netCDF"
+                             " file located at {}".format(variablename, pathname))
