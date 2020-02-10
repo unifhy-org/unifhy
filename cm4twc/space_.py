0716e794a09c16b365b54a632a1b94c5cfbaf502
Thibault Hallouin <thibault.hallouin@ncas.ac.uk>
2020-02-10 17:14:52 +0000

    redesigns space_ around cf.Field
    in view to work with cf-compliant datasets, this commit provides a first proper attempt at implementing a SpaceDomain, for a Grid in this case.
    
    The Grid class can work with proper lat/lon grids, or rotated grids for now. It is a subclass of cf.Field with constructs set for the space domain constructs, including lat/lon (or grid_lat/grid_lon), altitude (optional), and the coordinate reference in case of a rotated grid (coordinate reference providing the information necessary to convert grid_lat/grid_lon to lat_lon)
    
    The method to compare Fields on their space domain constructs will only compare their lat/lon (or grid_lat/grid_lon), altitude (optional), and the coordinate_conversion and datum of the coordinate_reference construct. It does not compare the whole of the coordinate_reference directly using its .equals() method because this also includes a check on the size of the collection of dimensions of each Field. This is not desirable to do this check as the time dimension is likely to be present in the Field but not in the SpaceDomain (for obvious reasons), and this also extends to the altitude dimension, which may be of size 1 in the Field, so that it allows to avoid defining it when instantiating the Grid.

--- cm4twc/space_.py
+++ cm4twc/space_.py
@@ -1,69 +1,175 @@
 import numpy as np
+import cf
 
 
-class SpaceDomain(object):
+class SpaceDomain(cf.Field):
     """
     Class to handle geospatial considerations.
     """
 
-    _supported_kinds = (
-        'grid',
-        'network',
-    )
+    def __init__(self):
 
-    def __init__(self, kind):
-
-        if kind in self._supported_kinds:
-            self.kind = kind
-        else:
-            raise TypeError("The SpaceDomain kind is not supported.")
-
-    def __eq__(self, other):
-
-        if not self.kind == other.kind:
-            return TypeError("The two SpaceDomain instances cannot be "
-                             "compared because they are of different kinds.")
+        super(SpaceDomain, self).__init__()
 
 
 class Grid(SpaceDomain):
 
-    def __init__(self, geo_coordinate_system, x, y):
+    def __init__(self, latitude_deg, longitude_deg, altitude_m=None,
+                 rotated=False, earth_radius_m=None,
+                 grid_north_pole_latitude_deg=None,
+                 grid_north_pole_longitude_deg=None):
 
-        super(Grid, self).__init__('grid')
+        super(Grid, self).__init__()
 
-        self.geo_coord_sys = geo_coordinate_system
-        if isinstance(x, np.ndarray):
-            self.x = x
-        else:
-            raise TypeError("The 'x' attribute of the Grid is not a "
-                            "numpy.ndarray.")
-        if isinstance(y, np.ndarray):
-            self.y = y
-        else:
-            raise TypeError("The 'y' attribute of the Grid is not a "
-                            "numpy.ndarray.")
+        # set the altitude construct
+        if altitude_m:
+            if not isinstance(altitude_m, np.ndarray):
+                altitude_m = np.asarray(altitude_m)
+            if altitude_m.ndim != 1:
+                raise RuntimeError("Error when initialising a {}: the "
+                                   "altitude array given is not "
+                                   "unidimensional".format(
+                                        self.__class__.__name__))
+
+            axis_alt = self.set_construct(cf.DomainAxis(len(altitude_m)))
+            self.set_construct(
+                cf.DimensionCoordinate(
+                    properties={'standard_name': 'altitude',
+                                'units': 'm'},
+                    data=cf.Data(altitude_m)),
+                axes=axis_alt)
+
+        # set the latitude construct
+        if not isinstance(latitude_deg, np.ndarray):
+            latitude_deg = np.asarray(latitude_deg)
+        if latitude_deg.ndim != 1:
+            raise RuntimeError("Error when initialising a {}: the "
+                               "latitude array given is not "
+                               "unidimensional".format(
+                                    self.__class__.__name__))
+        axis_lat = self.set_construct(cf.DomainAxis(len(latitude_deg)))
+        self.set_construct(
+            cf.DimensionCoordinate(
+                properties={
+                    'standard_name':
+                        'grid_latitude' if rotated else 'latitude',
+                    'units': 'degrees' if rotated else 'degrees_north'},
+                data=cf.Data(latitude_deg)),
+            axes=axis_lat
+        )
+
+        # set the longitude construct
+        if not isinstance(longitude_deg, np.ndarray):
+            longitude_deg = np.asarray(longitude_deg)
+        if longitude_deg.ndim != 1:
+            raise RuntimeError("Error when initialising a {}: the "
+                               "longitude array given is not "
+                               "unidimensional".format(
+                                    self.__class__.__name__))
+        axis_lon = self.set_construct(cf.DomainAxis(len(longitude_deg)))
+        self.set_construct(
+            cf.DimensionCoordinate(
+                properties={
+                    'standard_name':
+                        'grid_longitude' if rotated else 'longitude',
+                    'units': 'degrees' if rotated else 'degrees_east'},
+                data=cf.Data(longitude_deg)),
+            axes=axis_lon
+        )
+
+        # set the coordinate reference construct if relevant
+        if rotated:
+            if not earth_radius_m or not grid_north_pole_latitude_deg \
+                    or not grid_north_pole_longitude_deg:
+                raise RuntimeError("Error when initialising a {}: a rotated "
+                                   "grid is indicated, but one or more of the "
+                                   "required parameters is not provided: "
+                                   "earth_radius_m, grid_north_pole_latitude, "
+                                   "grid_north_pole_longitude".format(
+                                        self.__class__.__name__))
+            coord_conv = cf.CoordinateConversion(
+                parameters={'grid_mapping_name': 'rotated_latitude_longitude',
+                            'grid_north_pole_latitude':
+                                grid_north_pole_latitude_deg,
+                            'grid_north_pole_longitude':
+                                grid_north_pole_longitude_deg})
+            self.set_construct(
+                cf.CoordinateReference(
+                    datum=cf.Datum(
+                        parameters={'earth_radius': earth_radius_m}),
+                    coordinate_conversion=coord_conv)
+            )
+
+        # set a few attributes to avoid more sophisticated tests later on
+        self.has_altitude = True if altitude_m else False
+        self.is_rotated = rotated
 
     def __eq__(self, other):
 
-        if self.geo_coord_sys == other.geo_coord_sys \
-                and np.array_equal(self.x, other.x) \
-                and np.array_equal(self.y, other.y):
-            return True
+        if isinstance(other, Grid):
+            return self.is_matched_in(other)
         else:
-            return False
+            return TypeError("The {} instance cannot be compared to "
+                             "a {} instance.".format(self.__class__.__name__,
+                                                     other.__class__.__name__))
+
+    def __ne__(self, other):
+
+        return not self.__eq__(other)
+
+    def is_matched_in(self, variable, ignore_altitude=False):
+
+        # check whether latitude and longitude constructs are identical
+        if self.is_rotated:
+            # check if grid_latitude match, check if grid_longitude match and
+            # check if coordinate_reference match (by checking its
+            # coordinate_conversion and its datum separately, because
+            # coordinate_reference.equals() would also check the size of the
+            # collections of coordinates)
+            lat_lon = \
+                self.construct('grid_latitude').equals(
+                    variable.construct('grid_latitude', default=None),
+                    ignore_data_type=True) \
+                and self.construct('grid_longitude').equals(
+                    variable.construct('grid_longitude', default=None),
+                    ignore_data_type=True) \
+                and self.coordinate_reference(
+                    'rotated_latitude_longitude').coordinate_conversion.equals(
+                    variable.coordinate_reference(
+                        'rotated_latitude_longitude',
+                        default=None).coordinate_conversion) \
+                and self.coordinate_reference(
+                    'rotated_latitude_longitude').datum.equals(
+                    variable.coordinate_reference(
+                        'rotated_latitude_longitude',
+                        default=None).datum)
+        else:
+            # check if latitude match, check if longitude
+            lat_lon = \
+                self.construct('latitude').equals(
+                    variable.construct('latitude', default=None),
+                    ignore_data_type=True) \
+                and self.construct('longitude').equals(
+                    variable.construct('longitude', default=None),
+                    ignore_data_type=True)
+
+        # check whether altitude constructs are identical
+        if ignore_altitude:
+            alt = True
+        else:
+            if self.has_altitude:
+                alt = self.construct('altitude').equals(
+                    variable.construct('altitude', default=None),
+                    ignore_data_type=True)
+            else:
+                alt = True
+
+        return lat_lon and alt
 
 
 class Network(SpaceDomain):
 
-    def __init__(self, topology):
+    def __init__(self):
 
-        super(Network, self).__init__('network')
+        super(Network, self).__init__()
 
-        self.topo = topology  # some form of mapping type?
-
-    def __eq__(self, other):
-
-        if self.topo == other.topo:
-            return True
-        else:
-            return False
