5911be2bf06f8e941d7b330d1f9acc1d61ff1c6f
Thibault Hallouin <thibault.hallouin@ncas.ac.uk>
2020-02-11 12:33:00 +0000

    moves check on data/parameter + adds check on domain
    The existing checks on data and parameter being available / provided implemented at the Component level are moved to the Model level to avoid checking several times if Component is called several times (e.g. in case of parallelisation or else). The checks on inwards stays ay the component level for two reasons: first because the check on outwards is at this level too for now, and second because it is likely to be dealt with metaclasses in the future so these checks should become irrelevant.
    
    This commit also adds a check on the time and space domain compatibilities between the data and the components. It checks for time and space for driving data, and for space only for ancillary data.
    
    The API of Model.simulate is also slightly modified and the concept of "context" disappears and is replaced by "domain" (TimeDomian, SpaceDomain) and "data" (DataBase) being provided separately.

--- cm4twc/components/components.py
+++ cm4twc/components/components.py
@@ -15,15 +15,18 @@ class Component(object):
                  parameter_names, upwards, inwards, downwards, outwards):
 
         self.category = category
-        self.driving_data_names = driving_data_names if driving_data_names else ()
-        self.ancil_data_names = ancil_data_names if ancil_data_names else ()
-        self.parameter_names = parameter_names if parameter_names else ()
+        self.driving_data_names = \
+            driving_data_names if driving_data_names else ()
+        self.ancil_data_names = \
+            ancil_data_names if ancil_data_names else ()
+        self.parameter_names = \
+            parameter_names if parameter_names else ()
         self.upwards = upwards
         self.inwards = inwards
         self.downwards = downwards
         self.outwards = outwards
 
-    def __call__(self, tf, sd, db, **kwargs):
+    def __call__(self, td, sd, db, **kwargs):
 
         # check that all inward fluxes of info are provided
         if not all([i in kwargs for i in self.inwards]):
@@ -33,23 +36,9 @@ class Component(object):
                     self.category, self.__class__.__name__, self.inwards)
             )
 
-        # check that all parameters are provided
-        if not all([i in kwargs for i in self.parameter_names]):
-            raise RuntimeError(
-                "One or more parameters are missing in {} component '{}': "
-                "{} are all required.".format(
-                    self.category, self.__class__.__name__, self.parameter_names)
-            )
-
-        # check availability and collect required data from database
+        # collect required data from database
         for data in self.driving_data_names + self.ancil_data_names:
-            try:
-                kwargs[data] = db[data]
-            except KeyError:
-                raise KeyError(
-                    "There is no data '{}' available in the database "
-                    "for {} component '{}'.".format(
-                        data, self.category, self.__class__.__name__))
+            kwargs[data] = db[data].array
 
         # do something with time frame? with space domain?
         # (use them to check database has data for time and space required?)
--- cm4twc/model.py
+++ cm4twc/model.py
@@ -49,36 +49,50 @@ class Model(object):
             'openwater', given, inferred
         )
 
-    def simulate(self, surface_context, surface_parameters,
-                 subsurface_context, subsurface_parameters,
-                 openwater_context, openwater_parameters):
+    def simulate(self, surface_domain, surface_data, surface_parameters,
+                 subsurface_domain, subsurface_data, subsurface_parameters,
+                 openwater_domain, openwater_data, openwater_parameters):
         """
         DOCSTRING REQUIRED
         """
 
         # check that the context given for each component is a tuple
-        # (TimeDomain instance, SpaceDomain instance, DataBase instance)
-        self._check_modelling_context('surface', *surface_context)
-        self._check_modelling_context('subsurface', *subsurface_context)
-        self._check_modelling_context('openwater', *openwater_context)
+        # (TimeDomain instance, SpaceDomain instance)
+        self._check_component_domain(self._surface, *surface_domain)
+        self._check_component_domain(self._subsurface, *subsurface_domain)
+        self._check_component_domain(self._openwater, *openwater_domain)
 
-        if (surface_context[0] != subsurface_context[0]) \
-                or (surface_context[0] != openwater_context[0]):
+        if (surface_domain[0] != subsurface_domain[0]) \
+                or (surface_domain[0] != openwater_domain[0]):
             raise NotImplementedError(
                 "Currently, the modelling framework does not allow "
                 "for components to work on different TimeDomains.")
 
-        if (surface_context[1] != subsurface_context[1]) \
-                or (surface_context[1] != openwater_context[1]):
+        if (surface_domain[1] != subsurface_domain[1]) \
+                or (surface_domain[1] != openwater_domain[1]):
             raise NotImplementedError(
                 "Currently, the modelling framework does not allow "
                 "for components to work on different SpaceDomains.")
+
+        # check that the required parameters are provided
+        self._check_component_parameters(self._surface, surface_parameters)
+        self._check_component_parameters(self._subsurface, subsurface_parameters)
+        self._check_component_parameters(self._openwater, openwater_parameters)
+
+        # check that the required data is available in a DataBase instance
+        self._check_component_data(self._surface, surface_data,
+                                   *surface_domain)
+        self._check_component_data(self._subsurface, subsurface_data,
+                                   *subsurface_domain)
+        self._check_component_data(self._openwater, openwater_data,
+                                   *openwater_domain)
 
         interface_ = {}
 
         interface_.update(
             self._surface(
-                *surface_context,
+                *surface_domain,
+                db=surface_data,
                 **surface_parameters,
                 **interface_
             )
@@ -86,7 +100,8 @@ class Model(object):
 
         interface_.update(
             self._subsurface(
-                *subsurface_context,
+                *subsurface_domain,
+                db=subsurface_data,
                 **subsurface_parameters,
                 **interface_
             )
@@ -94,7 +109,8 @@ class Model(object):
 
         interface_.update(
             self._openwater(
-                *openwater_context,
+                *openwater_domain,
+                db=openwater_data,
                 **openwater_parameters,
                 **interface_
             )
@@ -216,37 +232,33 @@ class Model(object):
             return type
 
     @staticmethod
-    def _check_modelling_context(category, timedomain, spacedomain, database):
+    def _check_component_domain(component, timedomain, spacedomain):
         """
         The purpose of this method is to check that the elements in the
-        tuple given for a given component category are if the right type
-        (i.e. ([TimeDomain] instance, [SpaceDomain] instance,
-        [DataBase] instance))
+        tuple given for a given component category are of the right type
+        (i.e. ([TimeDomain] instance and [SpaceDomain] instance)
 
-        :param category: name of the component category being checked
-        (e.g. 'surface', 'subsurface', etc.)
-        :type category: str
-        :param timedomain: object being given as 1st argument during the
-        call of the [simulate] method for the given component category
-        :type timedomain: object being given as 2nd argument during the
-        call of the [simulate] method for the given component category
-        :param spacedomain: object being given as 3rd argument during the
-        call of the [simulate] method for the given component category
+        :param component: instance of the component whose domain is
+        being checked
+        :type component: Component
+        :param timedomain: object being given as 1st element of the domain
+        tuple during the call of the [simulate] method for the given component
+        :type timedomain: object
+        :param spacedomain: object being given as 2nd element of the domain
+        tuple during the call of the [simulate] method for the given component
         :type spacedomain: object
-        :param database:
-        :type database: object
 
         :return: None
         """
         if not isinstance(timedomain, TimeDomain):
-            raise TypeError("The 1st contextual item for the '{}' component "
+            raise TypeError("The 1st domain item for the '{}' component "
                             "must be an instance of {}.".format(
-                                category, TimeDomain.__name__))
+                                component.category, TimeDomain.__name__))
 
         if not isinstance(spacedomain, SpaceDomain):
-            raise TypeError("The 2nd contextual item for the '{}' component "
+            raise TypeError("The 2nd domain item for the '{}' component "
                             "must be an instance of {}.".format(
-                                category, TimeDomain.__name__))
+                                component.category, TimeDomain.__name__))
         else:
             if not isinstance(spacedomain, Grid):
                 raise NotImplementedError("The only {} subclass currently "
@@ -254,7 +266,98 @@ class Model(object):
                                           "{}.".format(SpaceDomain.__name__,
                                                        Grid.__name__))
 
+    @staticmethod
+    def _check_component_parameters(component, parameters):
+        """
+        The purpose of this method is to check that parameter values are given
+        for the corresponding component.
+
+        :param component: instance of the component whose domain is
+        being checked
+        :type component: Component
+        :param parameters: a dictionary containing the parameter values given
+        during the call of the [simulate] method for the given component
+        :type parameters: dict
+
+        :return: None
+        """
+
+        # check that all parameters are provided
+        if not all([i in parameters for i in component.parameter_names]):
+            raise RuntimeError(
+                "One or more parameters are missing in {} component '{}': "
+                "{} are all required.".format(
+                    component.category, component.__class__.__name__,
+                    component.parameter_names)
+            )
+
+    @staticmethod
+    def _check_component_data(component, database, timedomain, spacedomain):
+        """
+        The purpose of this method is to check that:
+            - the object given for the database is an instance of [DataBase]
+            - the database contains [Variable] instances for all the driving
+            and ancillary data the component requires
+            - the domain of each variable complies with the component's domain
+
+        :param component: instance of the component whose data is being checked
+        :type component: Component
+        :param database: object being given as the database for the given
+        component category
+        :type database: object
+        :param timedomain: instance of [TimeDomain] for the given
+        component category
+        :type timedomain: TimeDomain
+        :param spacedomain: instance of [SpaceDomain] for the given
+        component category
+        :type spacedomain: SpaceDomain
+
+        :return: None
+        """
+
+        # check that the data is an instance of DataBase
         if not isinstance(database, DataBase):
-            raise TypeError("The 3rd contextual item for the '{}' component "
+            raise TypeError("The database object given for the '{}' component "
                             "must be an instance of {}.".format(
-                                category, DataBase.__name__))
+                                component.category, DataBase.__name__))
+
+        # check driving data for time and space compatibility with component
+        for data in component.driving_data_names:
+            # check that all driving data are available in DataBase
+            if data not in database:
+                raise KeyError(
+                    "There is no data '{}' available in the database "
+                    "for the {} component '{}'.".format(
+                        data, component.category,
+                        component.__class__.__name__))
+            # check that the data and component time domains are compatible
+            if not timedomain.is_matched_in(database[data]):
+                raise ValueError(
+                    "The time domain of the data '{}' is not compatible with "
+                    "the time domain of the {} component '{}'.".format(
+                        data, component.category,
+                        component.__class__.__name__))
+            # check that the data and component space domains are compatible
+            if not spacedomain.is_matched_in(database[data]):
+                raise ValueError(
+                    "The space domain of the data '{}' is not compatible with "
+                    "the space domain of the {} component '{}'.".format(
+                        data, component.category,
+                        component.__class__.__name__))
+
+        # check ancillary data for space compatibility with component
+        for data in component.ancil_data_names:
+            # check that all ancillary data are available in DataBase
+            if data not in database:
+                raise KeyError(
+                    "There is no data '{}' available in the database "
+                    "for the {} component '{}'.".format(
+                        data, component.category,
+                        component.__class__.__name__))
+            # check that the data and component space domains are compatible
+            if not spacedomain.is_matched_in(database[data]):
+                raise ValueError(
+                    "The space domain of the data '{}' is not compatible with "
+                    "the space domain of the {} component '{}'.".format(
+                        data, component.category,
+                        component.__class__.__name__))
