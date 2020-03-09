5309d05b06ab1a2b47edd80678e966f8812102c3
Thibault Hallouin <thibault.hallouin@ncas.ac.uk>
2020-03-09 17:48:48 +0000

    adds squeeze option to Variable to get rid dimensions of length one
    It is not expected that a model works with length one dimensions (e.g. altitude ignored), but if this is the case, the vaalue of squeeze can be set to False to keep the dimensions of length one if necessary.

--- cm4twc/data_.py
+++ cm4twc/data_.py
@@ -30,39 +30,44 @@ class DataSet(MutableMapping):
 
     def __len__(self): return len(self.variables)
 
-    def append(self, *args, **kwargs): self.update(*args, **kwargs)
-
-    def update_with_cf_nc_file(self, pathname, name_mapping=None):
+    def update_with_cf_nc_file(self, pathname, name_mapping=None,
+                               squeeze=True):
 
         return self.update(
-            self._get_dict_variables_from_cf_nc_file(pathname, name_mapping)
+            self._get_dict_variables_from_cf_nc_file(pathname, squeeze,
+                                                     name_mapping)
         )
 
     @classmethod
-    def from_cf_nc_file(cls, pathname, name_mapping=None):
+    def from_cf_nc_file(cls, pathname, name_mapping=None, squeeze=True):
 
         return cls(
-            cls._get_dict_variables_from_cf_nc_file(pathname, name_mapping)
+            cls._get_dict_variables_from_cf_nc_file(pathname, squeeze,
+                                                    name_mapping)
         )
 
     @staticmethod
-    def _get_dict_variables_from_cf_nc_file(pathname, name_mapping=None):
+    def _get_dict_variables_from_cf_nc_file(pathname, squeeze, name_mapping):
         return {
             name_mapping[field.standard_name]
             if name_mapping and (field.standard_name in name_mapping)
-            else field.standard_name: Variable(source=field, copy=False)
+            else field.standard_name: Variable(source=field, copy=False,
+                                               squeeze=squeeze)
             for field in cf.read(pathname)
         }
 
 
 class Variable(cf.Field):
 
-    def __init__(self, source, copy=False):
+    def __init__(self, source, copy=False, squeeze=True):
 
         super(Variable, self).__init__(source=source, copy=copy)
 
+        if squeeze:
+            self.data.squeeze(inplace=True)
+
     @classmethod
-    def from_cf_nc_file(cls, variablename, pathname):
+    def from_cf_nc_file(cls, variablename, pathname, squeeze=True):
 
         try:
             field = cf.read(pathname).select_by_property(
@@ -75,7 +80,7 @@ class Variable(cf.Field):
 
         if field:
             if len(field) == 1:
-                return cls(source=field[0])
+                return cls(source=field[0], squeeze=squeeze)
             else:
                 raise UserWarning(
                     "AmbiguityWarning - There is more than one variable whose "
