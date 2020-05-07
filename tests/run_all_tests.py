cd90529eba794157fb6e6e049b720500b6e3e2b6
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2020-05-07 16:35:11 +0100

    adds a script to run all tests at once

---
+++ tests/run_all_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+if __name__ == '__main__':
+    test_loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+    all_tests = test_loader.discover('.', pattern='test_*.py')
+    test_suite.addTests(all_tests)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(test_suite)
