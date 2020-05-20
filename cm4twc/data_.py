e335258531785dc7496dba1c66529167e0fb1acb
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2020-05-20 13:50:01 +0200

    adds docstring for DataSet

--- cm4twc/data_.py
+++ cm4twc/data_.py
@@ -3,15 +3,33 @@ import cf
 
 
 class DataSet(MutableMapping):
-    """
-    Class behaving like a dictionary but restricting the values of
-    its items of type Variable.
+    """DataSet is a dictionary-like data structure which maps variable
+    names to `Field` objects. Namely, it allows to use custom variable
+    names instead of the standard_name attribute of `Field` to identify
+    them.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, dictionary=None):
+        """**Initialisation**
+
+        :Parameters:
+
+            dictionary: `dict`, optional
+                A dictionary with the variable names as keys referencing
+                `Field` objects.
+
+                *Parameter example:*
+                    ``dictionary={'rainfall': cf.Field()}``
+
+        **Examples**
+
+        >>> import cf
+        >>> ds = cm4twc.DataSet(dictionary={'rainfall': cf.Field()})
+        """
 
         self.variables = {}
-        self.update(dict(*args, **kwargs))  # use the 'free' update to set keys
+        if dictionary is not None:
+            self.update(dict(**dictionary))
 
     def __getitem__(self, key): return self.variables[key]
 
@@ -30,20 +48,106 @@ class DataSet(MutableMapping):
 
     def __len__(self): return len(self.variables)
 
-    def update_with_file(self, pathname, name_mapping=None,
+    def update_with_file(self, files, name_mapping=None,
                          squeeze=True, select=None, **kwargs):
+        """Append to the `DataSet` the variables that are contained in
+        the file(s) provided.
+
+        :Parameters:
+
+            files: (sequence of) `str`
+                A string or sequence of strings providing the netCDF
+                file names or directory names containing netCDF files
+                from which to read the variables.
+
+                *Parameter example:*
+                    ``files='tests/dummy/data/dummy_driving_data.nc'``
+                *Parameter example:*
+                    ``files=['tests/dummy/data/dummy_driving_data.nc',
+                             'tests/dummy/data/dummy_ancillary_data.nc'`
+
+            select: (sequence of) `str`, optional
+                A string or sequence of strings providing the identities
+                of the variables to read in the netCDF file. By default,
+                all variables in the netCDF file are read.
+
+                *Parameter example:*
+                    ``select=['rainfall_flux', 'snowfall_flux']``
+
+            name_mapping: `dict`, optional
+                A dictionary with the Field identities as keys and the
+                desired new name variables as values. If a Field is read
+                from the netCDF file, and its identity is not a key in
+                *name_mapping* (if provided), its standard_name
+                attribute is used instead.
+
+            squeeze: `bool`, optional
+                If True, dimension coordinates of the variables of size
+                1 are removed. The default value is True.
+
+        **Examples**
+
+        >>> ds = cm4twc.DataSet()
+        >>> td = cm4twc.update_with_file(
+        ...     files='tests/dummy_data/dummy_driving_data.nc',
+        ...     select='snowfall_flux'
+        ...)
+        """
 
         return self.update(
-            self._get_dict_variables_from_file(pathname, name_mapping,
+            self._get_dict_variables_from_file(files, name_mapping,
                                                squeeze, select, **kwargs)
         )
 
     @classmethod
-    def from_file(cls, pathname, name_mapping=None,
+    def from_file(cls, files, name_mapping=None,
                   squeeze=True, select=None, **kwargs):
+        """Initialise a `DataSet` from the variables contained in a
+        netCDF file.
+
+        :Parameters:
+
+            files: (sequence of) `str`
+                A string or sequence of strings providing the netCDF
+                file names or directory names containing netCDF files
+                from which to read the variables.
+
+                *Parameter example:*
+                    ``files='tests/dummy/data/dummy_driving_data.nc'``
+                *Parameter example:*
+                    ``files=['tests/dummy/data/dummy_driving_data.nc',
+                             'tests/dummy/data/dummy_ancillary_data.nc'`
+
+            select: (sequence of) `str`, optional
+                A string or sequence of strings providing the identities
+                of the variables to read in the netCDF file. By default,
+                all variables in the netCDF file are read.
+
+                *Parameter example:*
+                    ``select=['rainfall_flux', 'snowfall_flux']``
+
+            name_mapping: `dict`, optional
+                A dictionary with the Field identities as keys and the
+                desired new name variables as values. If a Field is read
+                from the netCDF file, and its identity is not a key in
+                *name_mapping* (if provided), its standard_name
+                attribute is used instead.
+
+            squeeze: `bool`, optional
+                If True, dimension coordinates of the variables of size
+                1 are removed. The default value is True.
+
+        **Examples**
+
+        >>> td = cm4twc.DataSet.from_file(
+        ...     files='tests/dummy_data/dummy_driving_data.nc',
+        ...     select=['rainfall_flux', 'snowfall_flux'],
+        ...     name_mapping={'rainfall_flux': 'rainfall'}
+        ...)
+        """
 
         return cls(
-            cls._get_dict_variables_from_file(pathname, name_mapping,
+            cls._get_dict_variables_from_file(files, name_mapping,
                                               squeeze, select, **kwargs)
         )
 
