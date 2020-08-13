93544ac68b33e0b3ed80b10cf8fede6639ffc9db
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2020-08-13 17:41:06 +0100

    updates Clock so it can be initialised with any number of components

--- cm4twc/_utils.py
+++ cm4twc/_utils.py
@@ -32,71 +32,54 @@ class Interface(MutableMapping):
 
 class Clock(object):
 
-    def __init__(self, surfacelayer_timedomain, subsurface_timedomain,
-                 openwater_timedomain, dumping_frequency=None):
+    def __init__(self, timedomains, dumping_frequency=None):
+        self.categories = tuple(timedomains)
         # determine temporal supermesh properties
         # (supermesh is the fastest component)
         supermesh_delta = min(
-            surfacelayer_timedomain.timedelta,
-            subsurface_timedomain.timedelta,
-            openwater_timedomain.timedelta
+            *[timedomains[cat].timedelta for cat in timedomains]
         )
         supermesh_length = max(
-            surfacelayer_timedomain.time.size,
-            subsurface_timedomain.time.size,
-            openwater_timedomain.time.size
+            *[timedomains[cat].time.size for cat in timedomains]
         )
         supermesh_step = supermesh_delta.total_seconds()
 
         # check that all timesteps are multiple integers of the supermesh step
-        sl_step = surfacelayer_timedomain.timedelta.total_seconds()
-        if not sl_step % supermesh_step == 0:
-            raise ValueError(
-                "timestep of surfacelayer component ({}s) not a multiple "
-                "integer of timestep of fastest component ({}s).".format(
-                    sl_step, supermesh_step))
-
-        ss_step = subsurface_timedomain.timedelta.total_seconds()
-        if not ss_step % supermesh_step == 0:
-            raise ValueError(
-                "timestep of subsurface component ({}s) not a multiple "
-                "integer of timestep of fastest component ({}s).".format(
-                    ss_step, supermesh_step))
-
-        ow_step = openwater_timedomain.timedelta.total_seconds()
-        if not ow_step % supermesh_step == 0:
-            raise ValueError(
-                "timestep of openwater component ({}s) not a multiple "
-                "integer of timestep of fastest component ({}s).".format(
-                    ow_step, supermesh_step))
+        steps = {}
+        for category in timedomains:
+            steps[category] = (
+                timedomains[category].timedelta.total_seconds()
+            )
+            if not steps[category] % supermesh_step == 0:
+                raise ValueError(
+                    "timestep of {} component ({}s) not a multiple "
+                    "integer of timestep of fastest component ({}s).".format(
+                        category, steps[category], supermesh_step))
 
         # get boolean arrays (switches) to determine when to run a given
         # component on temporal supermesh
-        self._sl_switch = np.zeros((supermesh_length,), dtype=bool)
-        sl_increment = int(sl_step // supermesh_step)
-        self._sl_switch[sl_increment-1::sl_increment] = True
-        self._sl_index_multiple = sl_increment
+        self.switches = {}
+        self.increments = {}
+        for category in timedomains:
+            self.switches[category] = np.zeros((supermesh_length,), dtype=bool)
+            sl_increment = int(steps[category] // supermesh_step)
+            self.switches[category][sl_increment - 1::sl_increment] = True
+            self.increments[category] = sl_increment
 
-        self._ss_switch = np.zeros((supermesh_length,), dtype=bool)
-        ss_increment = int(ss_step // supermesh_step)
-        self._ss_switch[ss_increment-1::ss_increment] = True
-        self._ss_index_multiple = ss_increment
-
-        self._ow_switch = np.zeros((supermesh_length,), dtype=bool)
-        ow_increment = int(ow_step // supermesh_step)
-        self._ow_switch[ow_increment-1::ow_increment] = True
-        self._ow_index_multiple = ow_increment
-
-        # determine whether model states dumping is required,and if so, when
-        self._dumping_switch = np.zeros((supermesh_length,), dtype=bool)
+        # determine whether model states dumping is required, and if so, when
+        self.switches['dumping'] = np.zeros((supermesh_length,), dtype=bool)
         if dumping_frequency is not None:
             # check that dumpy frequency is a multiple of
-            # slowest component's step
+            # the least common multiple across components
             dumping_delta = self._lcm_timedelta(
-                surfacelayer_timedomain.timedelta,
-                self._lcm_timedelta(subsurface_timedomain.timedelta,
-                                    openwater_timedomain.timedelta)
+                timedomains[self.categories[0]].timedelta,
+                timedomains[self.categories[1]].timedelta
             )
+            for category in self.categories[2:]:
+                dumping_delta = self._lcm_timedelta(
+                    dumping_delta,
+                    timedomains[category].timedelta
+                )
             dumping_step = dumping_frequency.total_seconds()
             if not dumping_step % dumping_delta.total_seconds() == 0:
                 raise ValueError(
@@ -108,15 +91,22 @@ class Clock(object):
             # get boolean arrays (switches) to determine when to dump the
             # component states on temporal supermesh
             dumping_increment = int(dumping_step // supermesh_step)
-            self._dumping_switch[0::dumping_increment] = True
+            self.switches['dumping'][0::dumping_increment] = True
         else:
             # get only one dump for the initial conditions
-            self._dumping_switch[0] = True
+            self.switches['dumping'][0] = True
 
         # set some time attributes
-        self.start_datetime = surfacelayer_timedomain.time.datetime_array[0]
-        self.end_datetime = surfacelayer_timedomain.time.datetime_array[-1]
         self.timedelta = supermesh_delta
+        self.length = supermesh_length
+
+        self.start_datetime = (
+            timedomains[self.categories[0]].time.datetime_array[0]
+        )
+        self.end_datetime = (
+            timedomains[self.categories[0]].time.datetime_array[-1]
+        )
+
         self.start_timeindex = 0
         self.end_timeindex = supermesh_length - 1
 
@@ -136,18 +126,9 @@ class Clock(object):
     def get_current_datetime(self):
         return self._current_datetime
 
-    def get_current_timeindex(self, component_type):
-        if component_type == 'surfacelayer':
-            return (self._current_timeindex //
-                    self._sl_index_multiple)
-        if component_type == 'subsurface':
-            return (self._current_timeindex //
-                    self._ss_index_multiple)
-        if component_type == 'openwater':
-            return (self._current_timeindex //
-                    self._ow_index_multiple)
-        else:
-            raise None
+    def get_current_timeindex(self, category):
+        return (self._current_timeindex //
+                self.increments[category])
 
     def __iter__(self):
         return self
@@ -161,10 +142,8 @@ class Clock(object):
             self._current_datetime += self.timedelta
 
             return (
-                self._sl_switch[index],
-                self._ss_switch[index],
-                self._ow_switch[index],
-                self._dumping_switch[index]
+                *(self.switches[cat][index] for cat in self.categories),
+                self.switches['dumping'][index]
             )
         else:
             raise StopIteration
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,50 +10,64 @@ class TestClockAPI(unittest.TestCase):
     def setUp(self):
         # daily
         self.td_a = cm4twc.TimeDomain(
-            timestamps=np.array([0, 1, 2, 3, 4, 5, 6]),
+            timestamps=np.array([0, 1, 2, 3, 4, 5, 6,
+                                 7, 8, 9, 10, 11, 12]),
             units='days since 1970-01-01 00:00:00',
             calendar='standard'
         )
         # 2-daily
         self.td_b = cm4twc.TimeDomain(
-            timestamps=np.array([0, 2, 4, 6]),
+            timestamps=np.array([0, 2, 4, 6, 8, 10, 12]),
             units='days since 1970-01-01 00:00:00',
             calendar='gregorian'
         )
         # 3-daily
         self.td_c = cm4twc.TimeDomain(
-            timestamps=np.array([0, 3, 6]),
+            timestamps=np.array([0, 3, 6, 9, 12]),
             units='days since 1970-01-01 00:00:00',
             calendar='gregorian'
         )
 
         self.d = timedelta(days=6)
 
-        self.exp_bool_a = [True, True, True, True, True, True]
-        self.exp_idx_a = [0, 1, 2, 3, 4, 5]
+        self.exp_bool_a = [True, True, True, True, True, True,
+                           True, True, True, True, True, True]
+        self.exp_idx_a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-        self.exp_bool_b = [False, True, False, True, False, True]
-        self.exp_idx_b = [0, 1, 2]
+        self.exp_bool_b = [False, True, False, True, False, True,
+                           False, True, False, True, False, True]
+        self.exp_idx_b = [0, 1, 2, 3, 4, 5]
 
-        self.exp_bool_c = [False, False, True, False, False, True]
-        self.exp_idx_c = [0, 1]
+        self.exp_bool_c = [False, False, True, False, False, True,
+                           False, False, True, False, False, True]
+        self.exp_idx_c = [0, 1, 2, 3]
 
-        self.exp_bool_d = [True, False, False, False, False, False]
+        self.exp_bool_d = [True, False, False, False, False, False,
+                           True, False, False, False, False, False]
 
     def test_clock_init(self):
-        clock = cm4twc._utils.Clock(surfacelayer_timedomain=self.td_a,
-                                    subsurface_timedomain=self.td_b,
-                                    openwater_timedomain=self.td_c)
+        clock = cm4twc._utils.Clock(
+            {'surfacelayer': self.td_a,
+             'subsurface': self.td_b,
+             'openwater': self.td_c},
+            dumping_frequency=self.d
+        )
 
-        self.assertEqual(clock._sl_switch.tolist(), self.exp_bool_a)
-        self.assertEqual(clock._ss_switch.tolist(), self.exp_bool_b)
-        self.assertEqual(clock._ow_switch.tolist(), self.exp_bool_c)
-        self.assertEqual(clock._dumping_switch.tolist(), self.exp_bool_d)
+        self.assertEqual(clock.switches['surfacelayer'].tolist(),
+                         self.exp_bool_a)
+        self.assertEqual(clock.switches['subsurface'].tolist(),
+                         self.exp_bool_b)
+        self.assertEqual(clock.switches['openwater'].tolist(),
+                         self.exp_bool_c)
+        self.assertEqual(clock.switches['dumping'].tolist(),
+                         self.exp_bool_d)
 
     def test_clock_iteration(self):
-        clock = cm4twc._utils.Clock(surfacelayer_timedomain=self.td_a,
-                                    subsurface_timedomain=self.td_b,
-                                    openwater_timedomain=self.td_c)
+        clock = cm4twc._utils.Clock(
+            {'surfacelayer': self.td_a,
+             'subsurface': self.td_b,
+             'openwater': self.td_c}
+        )
 
         out_bool_a, out_bool_b, out_bool_c = list(), list(), list()
         out_idx_a, out_idx_b, out_idx_c = list(), list(), list()
