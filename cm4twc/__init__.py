086ba64cc190164f8380b2bce49d43583a288f72
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2020-12-03 17:31:40 +0100

    replace ini file by yaml file for component dynamic import

--- cm4twc/__init__.py
+++ cm4twc/__init__.py
@@ -5,7 +5,7 @@ a Community Model for the Terrestrial Water Cycle
 # import python built-in packages, modules, classes, functions
 import sys
 from os import path
-from configparser import ConfigParser
+import yaml
 from importlib import import_module
 
 # import package's own modules, classes, functions
@@ -20,28 +20,27 @@ from .components import (surfacelayer, subsurface, openwater,
 from .settings import atol, rtol, decr, dtype_float
 
 # import modelling component classes defined in configuration file
-_cfg = ConfigParser()
-_cfg.read(path.join(path.abspath(path.dirname(__file__)),
-          'components', 'components.ini'))
+with open(path.join(path.abspath(path.dirname(__file__)),
+          'components', 'components.yml'), 'r') as f:
+    _cfg = yaml.load(f, yaml.FullLoader)
 
-for _component_type in ['surfacelayer', 'subsurface', 'openwater']:
-    if _component_type in _cfg:
-        for path_ in _cfg[_component_type]:
-            cls_name = _cfg[_component_type][path_]
-            # import module defined as key in configuration file
+for _category in ['surfacelayer', 'subsurface', 'openwater']:
+    if (_category in _cfg) and (_cfg[_category] is not None):
+        for _cls_name, _path in _cfg[_category].items():
+            # import module using path provided in configuration file
             try:
-                mod_ = import_module(path_, package=__name__)
+                _module = import_module(_path, package=__name__)
             except ImportError:
-                raise ImportError("The {} component '{}' could not be "
-                                  "imported.".format(_component_type, cls_name))
-            # get class defined as value in configuration file
+                raise ImportError("{} component '{}' could not be "
+                                  "imported.".format(_category, _cls_name))
+            # get class using name provided in configuration file
             try:
-                cls_ = getattr(mod_, cls_name)
+                _class = getattr(_module, _cls_name)
             except AttributeError:
-                raise AttributeError("The class '{}' could not be found in "
-                                     "the module '{}'.".format(cls_name,
-                                                               mod_.__name__))
+                raise AttributeError("class '{}' could not be found in "
+                                     "module '{}'.".format(_cls_name,
+                                                           _module.__name__))
             # assign class to relevant component module
             setattr(sys.modules['.'.join([__name__, 'components',
-                                          _component_type])],
-                    cls_name, cls_)
+                                          _category])],
+                    _cls_name, _class)
