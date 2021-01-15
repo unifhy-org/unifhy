403faeb83b96c2c10ee726afc6d789624a5d2594
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-01-15 10:51:09 +0100

    remove dynamic import of components
    Since contributors need to modify a file anyway to register their component to the framework, they may as well directly modify the relevant __init__.py rather than yet another yaml file.

--- cm4twc/__init__.py
+++ cm4twc/__init__.py
@@ -2,11 +2,6 @@
 a Community Model for the Terrestrial Water Cycle
 """
 
-# import python built-in packages, modules, classes, functions
-import sys
-from os import path
-import yaml
-from importlib import import_module
 
 # import package's own modules, classes, functions
 from .version import __version__
@@ -18,29 +13,3 @@ from .components import (surfacelayer, subsurface, openwater,
                          SurfaceLayerComponent, SubSurfaceComponent,
                          OpenWaterComponent, DataComponent, NullComponent)
 from .settings import atol, rtol, decr, dtype_float
-
-# import modelling component classes defined in configuration file
-with open(path.join(path.abspath(path.dirname(__file__)),
-          'components', 'components.yml'), 'r') as f:
-    _cfg = yaml.load(f, yaml.FullLoader)
-
-for _category in ['surfacelayer', 'subsurface', 'openwater']:
-    if (_category in _cfg) and (_cfg[_category] is not None):
-        for _cls_name, _path in _cfg[_category].items():
-            # import module using path provided in configuration file
-            try:
-                _module = import_module(_path, package=__name__)
-            except ImportError:
-                raise ImportError("{} component '{}' could not be "
-                                  "imported.".format(_category, _cls_name))
-            # get class using name provided in configuration file
-            try:
-                _class = getattr(_module, _cls_name)
-            except AttributeError:
-                raise AttributeError("class '{}' could not be found in "
-                                     "module '{}'.".format(_cls_name,
-                                                           _module.__name__))
-            # assign class to relevant component module
-            setattr(sys.modules['.'.join([__name__, 'components',
-                                          _category])],
-                    _cls_name, _class)
