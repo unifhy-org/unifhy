aa8dd0a73ba836fbd6c22e7e96759e8a4e8c313f
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-05-04 19:06:20 +0200

    fix bug when trying to record multi-division component states
    Since in the dummy tests, only single-division (i.e. scalar) states are used, this was not picked up, but the dimensions in the NetCDF files need to feature one for the state divisions is greater than one. This is now fixed, and multi-division (i.e. vector) states can be recorded.

--- cm4twc/components/_utils/records.py
+++ cm4twc/components/_utils/records.py
@@ -75,9 +75,10 @@ def _delta_to_frequency_str(delta):
 
 class Record(object):
 
-    def __init__(self, name, units, **kwargs):
+    def __init__(self, name, units, divisions=0, **kwargs):
         self.name = name
         self.units = units
+        self.divisions = divisions
         self.streams = []
 
 
@@ -139,6 +140,22 @@ class RecordStream(object):
         self.trigger = None
         self.trigger_tracker = None
 
+    def add_record(self, record, methods):
+        name = record.name
+        # store link to record object
+        self.records[name] = record
+        # store sequence of aggregation methods
+        methods_ = set()
+        for method in methods:
+            if method in _methods_map:
+                methods_.add(_methods_map[method])
+            else:
+                raise ValueError('method {} for record {} aggregation '
+                                 'unknown'.format(method, name))
+        self.methods[name] = methods_
+        # map this very stream in the record
+        record.streams.append(self)
+
     def initialise(self, timedomain, spacedomain, _skip_trackers=False):
         # check delta / timedomain resolution compatibility
         if (self.delta % timedomain.timedelta) != timedelta(seconds=0):
@@ -164,11 +181,20 @@ class RecordStream(object):
 
         # initialise record arrays for accumulating values
         self.trigger = 0
-        for name in self.records:
+        for name, record in self.records.items():
             self.array_trackers[name] = 0
-            arr = np.zeros((self.length, *spacedomain.shape), dtype_float())
+            d = record.divisions
+            if d > 1:
+                arr = np.zeros(
+                    (self.length, *spacedomain.shape, d), dtype_float()
+                )
+            else:
+                arr = np.zeros(
+                    (self.length, *spacedomain.shape), dtype_float()
+                )
             arr[:] = np.nan
             self.arrays[name] = arr
+
             # add on length of stream to the record trigger
             self.trigger += self.length
 
@@ -176,22 +202,6 @@ class RecordStream(object):
             # (re)initialise trackers
             self.time_tracker = 0
             self.trigger_tracker = 0
-
-    def add_record(self, record, methods):
-        name = record.name
-        # store link to record object
-        self.records[name] = record
-        # store sequence of aggregation methods
-        methods_ = set()
-        for method in methods:
-            if method in _methods_map:
-                methods_.add(_methods_map[method])
-            else:
-                raise ValueError('method {} for record {} aggregation '
-                                 'unknown'.format(method, name))
-        self.methods[name] = methods_
-        # map this very stream in the record
-        record.streams.append(self)
 
     def update_record(self, name, value):
         self.arrays[name][self.array_trackers[name], ...] = value
@@ -237,11 +247,17 @@ class RecordStream(object):
             b.calendar = self.timedomain.calendar
 
             for name, record in self.records.items():
+                d = record.divisions
+                if d > 1:
+                    f.createDimension('_'.join([name, 'divisions']), d)
+                    dims = ('time', *axes, '_'.join([name, 'divisions']))
+                else:
+                    dims = ('time', *axes)
+
                 # record variable
                 for method in self.methods[name]:
                     name_method = '_'.join([name, method])
-                    v = f.createVariable(name_method, dtype_float(),
-                                         ('time', *axes))
+                    v = f.createVariable(name_method, dtype_float(), dims)
                     v.standard_name = name
                     v.units = record.units
                     v.cell_methods = "time: {} over {}".format(
@@ -335,8 +351,15 @@ class RecordStream(object):
 
             # records
             for name, record in self.records.items():
-                s = f.createVariable(name, dtype_float(),
-                                     ('time', 'length', *axes),
+                d = record.divisions
+                if d > 1:
+                    f.createDimension('_'.join([name, 'divisions']), d)
+                    dims = ('time', 'length', *axes,
+                            '_'.join([name, 'divisions']))
+                else:
+                    dims = ('time', 'length', *axes)
+
+                s = f.createVariable(name, dtype_float(), dims,
                                      fill_value=9.9692099683868690E36)
                 s.standard_name = name
                 s.units = record.units
