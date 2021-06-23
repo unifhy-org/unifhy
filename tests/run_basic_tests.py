b6f1d5bde91dbf3dc0545bb6af76f5fedd699839
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-06-23 14:00:45 +0200

    return exit code 1 if unittests failed

--- tests/run_basic_tests.py
+++ tests/run_basic_tests.py
@@ -35,4 +35,7 @@ if __name__ == '__main__':
     test_suite.addTests(doctest.DocTestSuite(cm4twc._utils.exchanger))
 
     runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(test_suite)
+    result = runner.run(test_suite)
+
+    if not result.wasSuccessful():
+        exit(1)
