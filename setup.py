5b6ef0bb10d431b4d88e14a02fa1c4c578288007
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-08-18 17:29:15 +0200

    harmonise python versions supported with what is tested on GitHub

--- setup.py
+++ setup.py
@@ -62,14 +62,14 @@ setup(
         'Topic :: Scientific/Engineering :: Hydrology',
 
         'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
 
         'License :: OSI Approved :: BSD License',
 
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: 3.9',
     ],
 
     packages=find_packages(exclude=["docs*"]),
