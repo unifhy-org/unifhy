9e2eff7170d23c2efd97c9c5e18b5e49e5541a1d
Matt <mattjbr123@gmail.com>
2024-03-01 12:34:03 +0000

    Fixed typo in nutrientsurfacelayer cython pyx file #97

--- tests/tests/components/nutrientsurfacelayer/dummyc/dummyc.pyx
+++ tests/tests/components/nutrientsurfacelayer/dummyc/dummyc.pyx
@@ -7,7 +7,7 @@ cdef extern from "dummy.h":
                      double *state_b_m1)
 
     void run_(int ny, int nx, double *transfer_c, double *transfer_d,
-              double *transfer_f, double *driving_d, double *driving_e
+              double *transfer_f, double *driving_d, double *driving_e,
               double *driving_f, double *ancillary_e, double *state_a_m1,
               double *state_a_0, double *state_b_m1, double *state_b_0,
               double *transfer_a, double *transfer_b, double *transfer_h,
